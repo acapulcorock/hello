9a3167a3fd75610ad9d594d02e8c936e60b059b0
acapulcorock <economiaindustrial2022@gmail.com>
2022-02-07 04:28:00 +0000

    initial commit

---
+++ hello21.c
@@ -0,0 +1,24 @@
+/**
+ * autor palita tv
+ * date 06-02-2022
+ * un progama simple d ehola mundo
+ * /
+ * */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+
+int main (int argc, char **argv){
+
+
+    printf("eloy patlanid\n");
+      printf("colaborador\n");
+
+    return 0;
+
+
+
+
+
+}
